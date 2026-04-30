7c5f84ad7cb47d104f0e49da0fbae1b45b5d9aa9
liviahmariah <livia.maria1@escolar.ifrn.edu.br>
2026-04-30 14:07:54 -0300

    itens_ruins

--- SobrevivendoAdiasdeCao/Assets/Scripts/Item.cs
+++ SobrevivendoAdiasdeCao/Assets/Scripts/Item.cs
@@ -3,17 +3,33 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public int valor = 1;
-    public GameObject popupPrefab;
+    public bool itemBom = true;
+
+    public GameObject popupBom;
+    public GameObject popupRuim;
+
+    void Start()
+    {
+        valor = itemBom ? 1 : -1;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (!collision.CompareTag("Player")) return;
+
+        GameManager.instance.Coletar(valor);
+
+        GameObject popupEscolhido = itemBom ? popupBom : popupRuim;
+
+        if (popupEscolhido != null)
         {
-            GameManager.instance.Coletar(valor);
-
-            Instantiate(popupPrefab, transform.position, Quaternion.identity);
-
-            Destroy(gameObject);
+            Instantiate(
+                popupEscolhido,
+                transform.position,
+                Quaternion.identity
+            );
         }
+
+        Destroy(gameObject);
     }
 }
